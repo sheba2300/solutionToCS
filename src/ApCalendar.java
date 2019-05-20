284895601e62c176310b6231c61c4911fa7080d0
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2019-05-20 09:47:20 -0400

    made some methods apcalandar
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/ApCalendar.java
+++ src/ApCalendar.java
@@ -1,3 +1,36 @@
-public class ApCalendar {
-    
+public class ApCalendar
+{
+    private static boolean isLeapYear(int year)
+    {
+        return(year%4 == 0);
+    }
+
+    public static int numberOfLeapYears(int year1, int year2)
+    {
+        int count = 0;
+
+        for(int i = year1;i<=year2;i++)
+        {
+            if(isLeapYear(i))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int firstDayYear(int year)
+    {
+
+    }
+
+    private static int dayOfYear(int month, int day, int year)
+    {
+
+    }
+
+    public static int dayOfMonth(int month,int day,int year)
+    {
+
+    }
 }
